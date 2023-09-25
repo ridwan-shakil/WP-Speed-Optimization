37f895a02e715291e411312b8ca83815a43105b0
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-09-25 21:14:05 +0600

    Create Caching.php

---
+++ Caching.php
@@ -0,0 +1,20 @@
+<?php
+
+
+// ---------------------
+// Browser caching :
+// ---------------------
+// stores static files from your WordPress website, such as images, CSS, and JavaScript files, locally on their devices.
+// By doing so, subsequent visits to your site will retrieve these cached files from the browser's storage,
+
+// ---------------------
+// Page caching : 
+// ---------------------
+// stores a copy of the generated HTML page in the server's memory or disk, and then serves that copy to subsequent visitors instead of
+// generating the page from scratch each time.
+
+
+// ---------------------
+// Object caching :
+// ---------------------
+// stores frequently accessed database queries and complex data structures in memory, reducing the need to retrieve them from the database repeatedly. 
